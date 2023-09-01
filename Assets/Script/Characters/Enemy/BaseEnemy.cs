472d3ad58dab0e5c00826ffd97ee65c37cdd3f9a
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2023-09-01 20:03:22 -0300

    Implemented ListMovementEnemy

--- Assets/Script/Characters/Enemy/BaseEnemy.cs
+++ Assets/Script/Characters/Enemy/BaseEnemy.cs
@@ -2,29 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public delegate void EnemyKill(Enemy enemy);
-public class Enemy : Character
+public delegate void EnemyKill(BaseEnemy enemy);
+public class BaseEnemy : Character
 {
     public EnemyKill enemyKill;
 
+    //Positions
+    [SerializeField] private List<Vector2> _positionList= new List<Vector2>();
+    private int _indexListPosition = 0;
+    private int _currentIndexPosition;
+    private Vector2 _currentPosition;
+    private Vector2 _nextPosition;
+    private Vector2 _directionToNextPosition;
+
+    [SerializeField] private float _treshold = 0.0001f;
     //One more timeShoot
-    protected float p_timeShoot;
+    protected float _timeShoot;
 
     void Awake()
     {
         p_mainCamera = FindAnyObjectByType<Camera>();
         CameraLimit();
-        TimeShootSelection();
+        //TimeShootSelection();
+        _indexListPosition = Random.Range(0, _positionList.Count);
+        _nextPosition = _positionList[_indexListPosition];
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        PatrolMovement();
     }
 
     protected void TimeShootSelection()
     {
-        p_timeShoot = Random.Range(p_shootTimeRest, 5);
+        _timeShoot = Random.Range(p_shootTimeRest, 5);
     }
 
     protected override void Kill()
     {
         enemyKill?.Invoke(this);
         base.Kill();
+    }
+
+    protected void RandomIndexPosition()
+    {
+        _currentIndexPosition = _indexListPosition;
+        _indexListPosition = Random.Range(0, _positionList.Count);
+        if (_currentIndexPosition == _indexListPosition)
+        {
+            RandomIndexPosition();
+        }
+        _nextPosition = _positionList[_indexListPosition];
+    }
+
+    protected void PatrolMovement()
+    {
+        _currentPosition = transform.position;
+        _directionToNextPosition = _nextPosition - _currentPosition;
+        _directionToNextPosition.Normalize();
+
+        Move(_directionToNextPosition);
+
+        if ((_currentPosition - _nextPosition).magnitude <= _treshold)
+        {
+            RandomIndexPosition();
+        }
     }
 }
