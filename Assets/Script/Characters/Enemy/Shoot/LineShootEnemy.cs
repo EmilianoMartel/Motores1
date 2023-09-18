aa60424abcd02b6defeb06bd86ebc1c99de6f6eb
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2023-09-18 20:06:07 -0300

    Implemented LineShootEnemy

---
+++ Assets/Script/Characters/Enemy/Shoot/LineShootEnemy.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineShootEnemy : EnemyShoot
+{
+    private int _randomDirection;
+
+    protected override Vector2 GetRandomDirection()
+    {
+        ChooseDirection();
+        Vector2 direction = new Vector2(p_randomX, p_randomY);
+        p_randomY = 0;
+        p_randomX = 0;
+        return direction;
+    }
+
+    private void ChooseDirection()
+    {
+        _randomDirection = Random.Range(0, 2);
+        switch (_randomDirection)
+        {
+            case 0:
+                p_randomY = RandomDirection();
+                break;
+            case 1:
+                p_randomX = RandomDirection();
+                break;
+        }
+    }
+
+    private int RandomDirection()
+    {
+        int direction = Random.Range(0, 2); ;
+        switch (direction)
+        {
+            case 0:
+                direction = 1;
+                break;
+            case 1:
+                direction = -1;
+                break;
+        }
+        return direction;
+    }
+}
