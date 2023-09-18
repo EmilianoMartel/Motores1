ce8e2690da08f09ef47ebd63b93495c4bb0b09c6
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2023-09-18 20:07:22 -0300

    Update BaseEnemy

--- Assets/Script/Characters/Enemy/Shoot/BaseEnemy.cs
+++ Assets/Script/Characters/Enemy/Shoot/BaseEnemy.cs
@@ -10,6 +10,7 @@ public class BaseEnemy : Character
 
     //Managers
     [SerializeField] private EnemyMovement _enemyMovement;
+    [SerializeField] private EnemyShoot _enemyShoot;
 
     //Shooting
     [SerializeField] private float _maxTimeShoot = 5.0f;
@@ -20,13 +21,24 @@ public class BaseEnemy : Character
         p_mainCamera = FindAnyObjectByType<Camera>();
         CameraLimit();
         TimeShootSelection();
+        p_actualTime = 0;
     }
 
     private void Update()
     {
+        
         if (_enemyMovement != null)
         {
             _enemyMovement.Movement();
+        }
+        if (_enemyShoot != null)
+        {
+            p_actualTime += Time.deltaTime;
+            if (p_shootTimeRest < p_actualTime)
+            {
+                p_actualTime = 0;
+                _enemyShoot.Shoot();
+            }
         }
     }
 
