97b6cc403f688a31bfbc51de5dc1ca3e577355e7
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2023-09-18 20:06:35 -0300

    Implemented DiagonalShootEnemy

---
+++ Assets/Script/Characters/Enemy/Shoot/DiagonalShootEnemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiagonalShootEnemy : EnemyShoot
+{
+    protected override Vector2 GetRandomDirection()
+    {
+        ChooseDirection();
+        Vector2 direction = new Vector2(p_randomX, p_randomY);
+        p_randomY = 0;
+        p_randomX = 0;
+        return direction;
+    }
+
+    private void ChooseDirection()
+    {
+        p_randomY = RandomDirection();
+        p_randomX = RandomDirection();
+    }
+
+    private int RandomDirection()
+    {
+        int direction = Random.Range(0, 2); ;
+        switch (direction)
+        {
+            case 0:
+                direction = 1;
+                break;
+            case 1:
+                direction = -1;
+                break;
+        }
+        return direction;
+    }
+}
