a74c69efd8cd7116a543686a769559f9e672fe30
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2023-11-28 21:18:42 -0300

    Implemented PauseLogic

---
+++ Assets/Script/Ui/PausingLogic.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+using UnityEngine.InputSystem;
+
+public class PausingLogic : MonoBehaviour
+{
+    public Action isPausingEvent;
+    [SerializeField] private ManagerDataSourceSO _dataSource;
+
+    private void Awake()
+    {
+        if (!_dataSource)
+        {
+            Debug.LogError($"{name}: DataSource is null\n Check and assigned one\nDisabling component.");
+            enabled = false;
+            return;
+        }
+        _dataSource.pausingLogic = this;
+    }
+
+    public void PausingFunction(InputAction.CallbackContext inputContext)
+    {
+        if (inputContext.started)
+        {
+            isPausingEvent?.Invoke();
+        }
+    }
+}
